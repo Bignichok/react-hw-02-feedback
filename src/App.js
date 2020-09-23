3c1dc09ad757321a49e1a46ed2150ef5079ac778
Bignichok <bignik545@gmail.com>
2020-09-23 21:28:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
+import Section from "./components/Section/Section";
+import Statistics from "./components/Statistics/Statistics";
+import Notification from "./components/Notification/Notification.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = (type) => {
+    this.setState((state) => {
+      return {
+        [type]: state[type] + 1,
+      };
+    });
+  };
+
+  totalFeedBack = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedBack = this.totalFeedBack();
+    const positivePercentage =
+      totalFeedBack > 0 ? Math.floor((good / totalFeedBack) * 100) : 0;
+    return (
+      <div className="App">
+        <Section title={"Please leave feedback"}>
+          <FeedbackOptions onLeaveFeedback={this.onLeaveFeedback} />
+        </Section>
+        <Section title={"Statistics"}>
+          {totalFeedBack > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedBack}
+              positivePercentage={positivePercentage}
+            />
+          ) : (
+            <Notification message={"No feedback given"} />
+          )}
+        </Section>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const FeedbackOptions = ({ onLeaveFeedback }) => {
+  return (
+    <div>
+      <button type="button" onClick={() => onLeaveFeedback("good")}>
+        Good
+      </button>
+      <button type="button" onClick={() => onLeaveFeedback("neutral")}>
+        Neutral
+      </button>
+      <button type="button" onClick={() => onLeaveFeedback("bad")}>
+        Bad
+      </button>
+    </div>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  onLeaveFeedback: PropTypes.func,
+};
+
+export default FeedbackOptions;
