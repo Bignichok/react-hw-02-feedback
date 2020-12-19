37b6a254884eefdd32bf46376a91b7b2205b2b5d
Bignichok <bignik545@gmail.com>
2020-12-20 00:33:24 +0200

    refactoring

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Section = ({ title, children }) => {
-  return (
-    <section>
-      <h2>{title}</h2>
-      {children}
-    </section>
-  );
-};
+const Section = ({ title, children }) => (
+  <section>
+    <h2>{title}</h2>
+    {children}
+  </section>
+);
 
 Section.propTypes = {
   title: PropTypes.string.isRequired,
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,29 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styles from './Statistics.module.css'
 
+import styles from "./Statistics.module.css";
 
-const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
-  return (
-    <ul>
-      <li>
-        <p>Good: {good}</p>
-      </li>
-      <li>
-        <p>Neutral: {neutral}</p>
-      </li>
-      <li>
-        <p>Bad: {bad}</p>
-      </li>
-      <li>
-        <p>Total: {total}</p>
-      </li>
-      <li>
-        <p>Positive feedback: {positivePercentage}%</p>
-      </li>
-    </ul>
-  );
-};
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+  <ul>
+    <li>
+      <p>Good: {good}</p>
+    </li>
+    <li>
+      <p>Neutral: {neutral}</p>
+    </li>
+    <li>
+      <p>Bad: {bad}</p>
+    </li>
+    <li>
+      <p>Total: {total}</p>
+    </li>
+    <li>
+      <p>Positive feedback: {positivePercentage}%</p>
+    </li>
+  </ul>
+);
 
 Statistics.propTypes = {
   good: PropTypes.number.isRequired,
